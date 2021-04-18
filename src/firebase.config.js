aaa12b730cf8e3fe4de0930736e8f5d98dd600e4
nabilahaffryda <sehungantengs@gmail.com>
2021-04-18 23:53:17 +0700

    week 10

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAVNFxzA22X0CLZy7HlrEGRmv94u-oMBDY",
+    authDomain: "week10-login.firebaseapp.com",
+    projectId: "week10-login",
+    storageBucket: "week10-login.appspot.com",
+    messagingSenderId: "342902427162",
+    appId: "1:342902427162:web:065133868bb9270d850ba3",
+    measurementId: "G-H8PB3NM6TR"
+  };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router'
+import routes from './routes'
+import Header from './Header'
+import "./styles.css"
+import firebase from "firebase"
+import firebaseConfig from "./firebase.config"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp(firebaseConfig)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const AuthContext = React.createContext(null)
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false)
+
+  return (
+    <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div>
+        <BrowserRouter>
+          <Header/>
+          <Switch>
+            {routes.map(route => (
+              <Route
+                key = {route.path}
+                path = {route.path}
+                exact = {route.exact}
+                component = {route.main}
+              />
+            ))}
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App/>, rootElement)
